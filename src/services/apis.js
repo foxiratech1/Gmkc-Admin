d95f2b6d59150655b6c4f398bcb66b0acbf73a1f
foxiratech1 <foxiratech@gmail.com>
2025-03-12 12:27:59 +0530

    staris in shipment

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+// const BASE_URL = "http://localhost:5000";
 //
 export const authendpoints = {
   SIGNIN_API: BASE_URL + "/api/admin/login",
